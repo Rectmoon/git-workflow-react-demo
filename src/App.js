cd68d6a197d4c2b145894f7a0f66d3c337f18cfe
Rectmoon <15692087320@163.com>
2020-01-10 00:51:57 +0800

    ~~~

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>so pretty</p>
+        <p>The deploy key is ready!</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
